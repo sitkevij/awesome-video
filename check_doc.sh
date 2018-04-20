9cba7ab529c5fa62653315f2e54edce637eb01bb
sitkevij <sitkevij@gmail.com>
2018-04-20 10:04:58 -0700

    add error checking

--- check_doc.sh
+++ check_doc.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+date
 grep -E -orh --line-buffered "(http(s)?://){1}[^'\\)]+" README.md >urls.tmp
 while read -r LINE; do
     curl -o /dev/null --silent --progress-bar --head --write-out '%{http_code} %{time_starttransfer} %{url_effective}\n' "$LINE" >> urls_result.tmp
     sleep 2
 done < urls.tmp
+date
+if cat urls_result.tmp | grep '000 ' ;
+then
+  echo "URL check error."
+  exit 1
+fi
+
+
